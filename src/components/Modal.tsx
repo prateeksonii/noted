548ec3a50f25e3bd9e8617217cd5a7c7d36e41ce
Prateek Soni <prateek_soni@outlook.com>
2022-10-01 03:09:37 +0530

    move note creation to modal
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

---
+++ src/components/Modal.tsx
@@ -0,0 +1,79 @@
+import { Dialog } from "@headlessui/react";
+import {
+  Dispatch,
+  FC,
+  FormEventHandler,
+  SetStateAction,
+  useState,
+} from "react";
+import { MdAdd, MdCheck } from "react-icons/md";
+import { Note } from "../types/notes";
+
+interface ModalProps {
+  notes: Note[];
+  setNotes: Dispatch<SetStateAction<Note[]>>;
+}
+
+const Modal: FC<ModalProps> = ({ notes, setNotes }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [note, setNote] = useState("");
+
+  const handleAddNote = () => {
+    if (note === "") return;
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      note,
+      createdAt: Date.now().toString(),
+    };
+
+    setNotes([newNote, ...notes]);
+    setNote("");
+    setIsOpen(false);
+  };
+  return (
+    <>
+      {isOpen ? (
+        <button
+          className="absolute -top-4 -right-0 bottom-auto z-10 grid h-12 w-12 place-items-center items-center gap-1 rounded-full bg-green-600 !p-0 py-2 px-4 font-medium transition-all hover:bg-sky-800"
+          onClick={handleAddNote}
+        >
+          <MdCheck className="text-2xl" />
+        </button>
+      ) : (
+        <button
+          className="absolute bottom-1 right-1 flex items-center gap-1 rounded-full bg-sky-600 py-2 px-4 font-medium transition-all hover:bg-sky-800"
+          onClick={() => setIsOpen(true)}
+        >
+          <>
+            <MdAdd />
+            <span>Add note</span>
+          </>
+        </button>
+      )}
+      <Dialog
+        open={isOpen}
+        onClose={handleAddNote}
+        className="fixed top-20 bottom-10 left-10 right-10 rounded border bg-zinc-800 p-4"
+      >
+        <Dialog.Panel className="flex h-full flex-col">
+          <Dialog.Title className="text-2xl font-medium">
+            What's on your mind?
+          </Dialog.Title>
+          <div className="my-2 h-full">
+            <textarea
+              name="note"
+              id="note"
+              className="h-full w-full rounded border-none border-zinc-700 bg-inherit outline-none transition-all duration-1000 placeholder:text-zinc-500"
+              placeholder="Take a note..."
+              onChange={(e) => setNote(e.target.value)}
+              value={note}
+            />
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </>
+  );
+};
+
+export default Modal;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import type { NextPage } from "next";
 import { FormEventHandler, useEffect, useState } from "react";
-import { MdOutlineStickyNote2 } from "react-icons/md";
+import { MdAdd, MdCheck, MdOutlineStickyNote2 } from "react-icons/md";
 import { AnimateSharedLayout, motion } from "framer-motion";
-
-type Note = {
-  id: number;
-  note: string;
-  createdAt: string;
-};
+import { Dialog } from "@headlessui/react";
+import { Note } from "../types/notes";
+import Modal from "../components/Modal";
 
 const IndexPage: NextPage = () => {
   const [notes, setNotes] = useState<Note[]>([]);
-
-  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target as HTMLFormElement);
-    const note = formData.get("note") as string;
-
-    const newNote: Note = {
-      id: notes.length + 1,
-      note,
-      createdAt: Date.now().toString(),
-    };
-
-    setNotes([newNote, ...notes]);
-  };
 
   useEffect(() => {
     const notes = localStorage.getItem("noted_notes");
@@ -41,39 +23,28 @@ const IndexPage: NextPage = () => {
   }, [notes]);
 
   return (
-    <div className="container mx-auto p-4">
-      <nav className="flex items-center gap-2">
+    <div className="container mx-auto flex h-screen flex-col p-4">
+      <nav className="flex h-16 items-center gap-2">
         <MdOutlineStickyNote2 className="h-10 w-10" />
         <span className="text-2xl font-medium">Noted</span>
       </nav>
 
-      <main className="my-8">
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="note"
-            id="note"
-            className="w-full rounded border-zinc-700 bg-zinc-900 placeholder:text-zinc-500"
-            placeholder="Take a note..."
-          />
-          <input type="submit" value="" />
-        </form>
-
+      <main className="relative h-full">
         <AnimateSharedLayout>
           <motion.div className="mt-4 flex flex-col gap-4" layout>
             {notes.map((note) => (
               <motion.div
+                key={note.id}
                 layout
                 initial={{ opacity: 0, translateY: "-100%" }}
                 animate={{ opacity: 1, translateY: "0" }}
               >
-                <div key={note.id} className="rounded bg-zinc-700 p-4">
-                  {note.note}
-                </div>
+                <div className="rounded bg-zinc-700 p-4">{note.note}</div>
               </motion.div>
             ))}
           </motion.div>
         </AnimateSharedLayout>
+        <Modal notes={notes} setNotes={setNotes} />
       </main>
     </div>
   );
