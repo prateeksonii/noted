87f8ce6bbbb78b2d8bde350a55e016511e9d69d0
Prateek Soni <prateek_soni@outlook.com>
2022-10-02 01:49:58 +0530

    should fix mobile height
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const IndexPage: NextPage = () => {
   }, [notes]);
 
   return (
-    <div className="container mx-auto flex h-screen flex-col p-4">
+    <div className="container mx-auto flex h-full flex-col p-4">
       <nav className="flex h-16 items-center gap-2">
         <MdOutlineStickyNote2 className="h-10 w-10" />
         <span className="text-2xl font-medium">Noted</span>
